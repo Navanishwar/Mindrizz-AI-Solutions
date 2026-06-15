7541bac0575ccadec1221f8d5b454743b51188f4
Navanish <navaneshwarreddy1614@gmail.com>
2026-06-15 17:41:50 +0530

    Added contact form and lead management system

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+
+    email = models.EmailField()
+
+    phone = models.CharField(max_length=20)
+
+    company = models.CharField(max_length=100)
+
+    requirement = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- website/views.py
+++ website/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import LeadForm
 
 def home(request):
-    return render(request, 'home.html')
+
+    if request.method == 'POST':
+
+        form = LeadForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect('/')
+
+    else:
+
+        form = LeadForm()
+
+    return render(
+        request,
+        'home.html',
+        {'form': form}
+    )
