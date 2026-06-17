175b87f214fd3e414f5f97595b7043256585bfb6
Navanish <navaneshwarreddy1614@gmail.com>
2026-06-17 21:48:21 +0530

    Final PostgreSQL and lpcalDB setup

--- mindrizz_site/settings.py
+++ mindrizz_site/settings.py
@@ -83,12 +83,20 @@ WSGI_APPLICATION = 'mindrizz_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASES = {
-    'default': 
-        dj_database_url.config(
-            default=os.getenv("DATABASE_URL")
-        ),
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
 
 
