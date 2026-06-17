2bb554ca235da95ba6ae6222ff9e107a411a4657
Navanish <navaneshwarreddy1614@gmail.com>
2026-06-17 18:53:21 +0530

    Connect PsotgreSQL database

--- mindrizz_site/settings.py
+++ mindrizz_site/settings.py
@@ -86,8 +86,9 @@ WSGI_APPLICATION = 'mindrizz_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        dj_database_url.config(
+            default=os.getenv("DATABASE_URL")
+        ),
     }
 }
 
