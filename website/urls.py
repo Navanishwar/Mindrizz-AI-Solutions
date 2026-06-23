7e920eceab0a41beda54330ea633d8feb781bcd7
Navanish Reddy <navanishreddy373@gmail.com>
2026-06-24 04:16:10 +0530

    Implement SEO, responsiveness, form handling and external link fixes

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic.base import TemplateView, RedirectView
 from .views import home, contact, services, about
 
 urlpatterns = [
@@ -6,4 +7,7 @@ urlpatterns = [
     path('services/', services, name='services'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name='robots_txt'),
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml"), name='sitemap_xml'),
+    path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.png', permanent=True), name='favicon_ico'),
 ]
